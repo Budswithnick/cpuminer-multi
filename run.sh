6b95348b1ec11657d8d00767e822cb7684b32e20
Trey <treymojo@gmail.com>
2019-03-01 23:49:53 -0500

    Got bitcoin mining. Now I just need to make the dockerfile run automatically in portainer!

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-./cpuminer -a sha256d -o stratum+tcp://us-east.stratum.slushpool.com:3333 -u  14vQBecQQg6mqniP94ustDoFp43ih9ingF -p Damien()Budnick1 c=BTC
+./cpuminer -a sha256d -o stratum+tcp://us-east.stratum.slushpool.com:3333 -u budswithnick.worker1 
